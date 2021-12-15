d14dcfc252652885ed3bc618a3185fb5447c59aa
protolambda <proto@protolambda.com>
2021-12-15 19:16:02 +0100

    l1 downloader: refactor, de-dup requests better, fix receipt download workers management

--- opnode/l1/tracker.go
+++ opnode/l1/tracker.go
@@ -2,8 +2,10 @@ package l1
 
 import (
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/log"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -37,6 +39,48 @@ type BlockID struct {
 	Number uint64
 }
 
+type ChainReaderWithReceipts interface {
+	ethereum.ChainReader
+	ethereum.TransactionReader
+}
+
+type blockAndReceipts struct {
+	// Track if we finished (with error or not). May increment >1 when other sub-tasks fail.
+	// First field, aligned atomic changes.
+	finished uint32
+
+	// Count receipts to track status
+	// First field of struct for memory aligned atomic access
+	DownloadedReceipts uint32
+
+	Block *types.Block
+	// allocated in advance, one for each transaction, nil until downloaded
+	Receipts []*types.Receipt
+
+	// stop fetching if this context is dead
+	ctx context.Context
+
+	// for other duplicate requests to get the result
+	feed event.Feed
+}
+
+func (bl *blockAndReceipts) Finish(err error) {
+	if atomic.AddUint32(&bl.finished, 1) == 1 {
+		bl.feed.Send(err)
+	}
+}
+
+type receiptTask struct {
+	BlockHash common.Hash
+	TxHash    common.Hash
+	TxIndex   uint64
+	// Count the attempts we made to fetch this receipt. Block as a whole fails if we tried to many times.
+	Retry uint64
+	// Avoid concurrent Downloader cache access and pruning edge cases with receipts
+	// Keep a pointer to insert the receipt at
+	Dest *blockAndReceipts
+}
+
 type L1Tracker struct {
 	sync.Mutex
 
@@ -47,6 +91,17 @@ type L1Tracker struct {
 
 	// feed of BlockID
 	headChanges event.Feed
+
+	// cache of ongoing/completed block tasks: block hash -> block
+	cache     map[common.Hash]*blockAndReceipts
+	cacheLock sync.Mutex
+
+	receiptTasks       chan *receiptTask
+	receiptWorkers     []ethereum.Subscription
+	receiptWorkersLock sync.Mutex
+
+	// TODO
+	chr ChainReaderWithReceipts
 }
 
 func (l1t *L1Tracker) HeadSignal(id BlockID) {
@@ -167,6 +222,113 @@ func (l1t *L1Tracker) Prune(number uint64) {
 	}
 }
 
+func (l1t *L1Tracker) DownloadBlock(ctx context.Context, id BlockID) (*blockAndReceipts, error) {
+	// check if we are already working on it
+	l1t.cacheLock.Lock()
+	bnr, ok := l1t.cache[id.Hash]
+	if !ok {
+		bnr = new(blockAndReceipts)
+		l1t.cache[id.Hash] = bnr
+
+		// pull the block in the background
+		go func() {
+			ctx, _ := context.WithTimeout(ctx, time.Second*10)
+			bl, err := l1t.chr.BlockByHash(ctx, id.Hash)
+			if err != nil {
+				bnr.Finish(fmt.Errorf("failed to download block %s: %v", id.Hash, err))
+				return
+			}
+
+			txs := bl.Transactions()
+			bnr.Block = bl
+			bnr.Receipts = make([]*types.Receipt, len(txs), len(txs))
+
+			for i, tx := range txs {
+				l1t.receiptTasks <- &receiptTask{BlockHash: id.Hash, TxHash: tx.Hash(), TxIndex: uint64(i), Dest: bnr}
+			}
+		}()
+	}
+	l1t.cacheLock.Unlock()
+
+	ch := make(chan error)
+	sub := bnr.feed.Subscribe(ch)
+	select {
+	case err := <-ch:
+		if err != nil {
+			return nil, err
+		}
+		return bnr, nil
+	case err := <-sub.Err():
+		return nil, err
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+const maxReceiptRetry = 5
+
+func (l1t *L1Tracker) newReceiptWorker() ethereum.Subscription {
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		for {
+			select {
+			case task := <-l1t.receiptTasks:
+				// scheduled tasks may be stale if other receipts of the block failed too many times
+				if task.Dest.finished > 0 { // no need for locks, a very rare stale download does not hurt
+					continue
+				}
+				// limit fetching to the task as a whole, and constrain to 10 seconds for receipt itself
+				ctx, _ := context.WithTimeout(task.Dest.ctx, time.Second*10)
+				receipt, err := l1t.chr.TransactionReceipt(ctx, task.TxHash)
+				if err != nil {
+					// if a single receipt fails out of the whole block, we can retry a few times.
+					if task.Retry >= maxReceiptRetry {
+						// Failed to get the receipt too many times, block fails!
+						task.Dest.Finish(fmt.Errorf("failed to download receipt again, and reached max %d retries: %v", maxReceiptRetry, err))
+						continue
+					} else {
+						task.Retry += 1
+						select {
+						case l1t.receiptTasks <- task:
+							// all good, retry scheduled successfully
+						default:
+							// failed to schedule, too much receipt work, stop block to relieve pressure.
+							task.Dest.Finish(fmt.Errorf("receipt downloader too busy, not downloading receipt again (%d retries): %v", task.Retry, err))
+							continue
+						}
+						continue
+					}
+				}
+				task.Dest.Receipts[task.TxIndex] = receipt
+				// We count the receipts we have so far (atomic, avoid parallel counting race condition)
+				total := atomic.AddUint32(&task.Dest.DownloadedReceipts, 1)
+				if total == uint32(len(task.Dest.Receipts)) {
+					// block completed without error!
+					task.Dest.Finish(nil)
+					continue
+				}
+				// task completed, but block is not complete without other receipt tasks finishing first
+			case <-quit:
+				return nil
+			}
+		}
+	})
+}
+
+func (l1t *L1Tracker) AddReceiptWorkers(n int) {
+	l1t.receiptWorkersLock.Lock()
+	defer l1t.receiptWorkersLock.Unlock()
+	if n < 0 {
+		for i := 0; i < -n; i++ {
+			last := len(l1t.receiptWorkers) - 1
+			l1t.receiptWorkers[last].Unsubscribe()
+			l1t.receiptWorkers = l1t.receiptWorkers[:last]
+		}
+	}
+	for i := 0; i < n; i++ {
+		l1t.receiptWorkers = append(l1t.receiptWorkers, l1t.newReceiptWorker())
+	}
+}
+
 // L1Maintainer is a *cache* of chain connections.
 // It helps quickly decide which blocks to pull down, and resolves reorgs.
 // It does not track the past *processed* L1 blocks, it is robust against change.
@@ -184,6 +346,8 @@ type L1Maintainer interface {
 	Prune(number uint64)
 	// WatchHeads subscribes to get head updates
 	WatchHeads(chan<- BlockID) ethereum.Subscription
+	// DownloadBlock downloads a L1 block with receipts (cached)
+	DownloadBlock(ctx context.Context, id BlockID) (*blockAndReceipts, error)
 }
 
 // SubL1Node wraps a new-head subscription from ChainReader to feed the given L1Maintainer
@@ -236,7 +400,7 @@ type LocalView interface {
 	// - If id.Number == LastL1().Number + 1: extend chain if parent matches, or reorg
 	// - If id.Number > LastL1().Number + 1: far out of sync, including possible reorg,
 	//   try catching up through other means if possible, headers are being synced to find 1-by-1 sync start.
-	ProcessL1(id BlockID) // TODO: maybe add ChainMode arg, but keeping simplicity here is nice too
+	ProcessL1(l1m L1Maintainer, id BlockID) // TODO: maybe add ChainMode arg, but keeping simplicity here is nice too
 }
 
 // L1HeaderSync fetches missing data for L1 maintainer, and detects head changes / reorgs,
@@ -308,13 +472,14 @@ func L1HeaderSync(ctx context.Context, chr ethereum.ChainReader, l1m L1Maintaine
 			case <-pullTicker.C:
 				lastLocal := local.LastL1()
 				nextLocal, fastNext := syncStep(lastLocal)
+				if lastLocal != nextLocal {
+					local.ProcessL1(l1m, nextLocal)
+				}
+				// after completing processing, schedule the next step
 				if fastNext {
 					pullTicker.Reset(pullHot)
 				} else {
 					pullTicker.Reset(pullCold)
-				}
-				if lastLocal != nextLocal {
-					local.ProcessL1(nextLocal)
 				}
 			case <-pruneTicker.C:
 				height := local.LastL1().Number
