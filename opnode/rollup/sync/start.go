654770dcb3d054810eea0dc1bc37ec0366058e44
Nicolas "Norswap" Laurent <norswap@gmail.com>
2022-04-08 16:40:33 +0200

    fix bug (L1 origin head with non-canonical ancestors)
    This happened in the scenario where `l2ahead == true` (L1 origin of start is
    ahead of know L1 head) and `start` had an ancestor whose L1 origin was known not
    to be canonical.
    
    Example: L1 head is at block X. `start` has an L1 origin with number X + 1 but its
    parent L2 block has an L1 origin with number X whose blockhash is not canonical.
    In this case, the algorithm still returned `start` as the unsafe L1 head, which is
    incorrect.
    
    This also touches a bunch of comments to generally improve understanbility.

--- opnode/rollup/sync/start.go
+++ opnode/rollup/sync/start.go
@@ -1,15 +1,16 @@
 // The sync package is responsible for reconciling L1 and L2.
 //
-// The ethereum chain is a DAG of blocks with the root block being the genesis block. At any given
-// time, the head (or tip) of the chain can change if an offshoot of the chain has a higher number.
-// This is known as a re-organization of the canonical chain. Each block points to a parent block
-// and the node is responsible for deciding which block is the head and thus the mapping from block
-// number to canonical block.
+// The Ethereum chain is a DAG of blocks with the root block being the genesis block. At any given
+// time, the head (or tip) of the chain can change if an offshoot/branch of the chain has a higher
+// number. This is known as a re-organization of the canonical chain. Each block points to a parent
+// block and the node is responsible for deciding which block is the head and thus the mapping from
+// block number to canonical block.
 //
-// The Optimism chain has similar properties, but also retains references to the ethereum chain.
-// Each Optimism block retains a reference to an L1 block (its "L1 origin", i.e. L1 block associated
-// with the epoch that the L2 block belongs to) and to its parent L2 block. The L2 chain node must
-// satisfy the following validity rules:
+// The Optimism (L2) chain has similar properties, but also retains references to the Ethereum (L1)
+// chain. Each L2 block retains a reference to an L1 block (its "L1 origin", i.e. L1 block
+// associated with the epoch that the L2 block belongs to) and to its parent L2 block. The L2 chain
+// node must satisfy the following validity rules:
+//
 //     1. l2block.number == l2block.l2parent.block.number + 1
 //     2. l2block.l1Origin.number >= l2block.l2parent.l1Origin.number
 //     3. l2block.l1Origin is in the canonical chain on L1
@@ -20,13 +21,15 @@
 //
 // When one of these changes occurs, the rollup node needs to determine what the new L2 head blocks
 // should be. We track two L2 head blocks:
+//
 //     - The *unsafe L2 block*: This is the highest L2 block whose L1 origin is a plausible (1)
 //       extension of the canonical L1 chain (as known to the opnode).
 //     - The *safe L2 block*: This is the highest L2 block whose epoch's sequencing window is
 //       complete within the canonical L1 chain (as known to the opnode).
 //
 // (1) Plausible meaning that the blockhash of the L2 block's L1 origin (as reported in the L1
-//     Attributes deposit within the L2 block) is not canonical at another height in the L1 chain.
+//     Attributes deposit within the L2 block) is not canonical at another height in the L1 chain,
+//     and the same holds for all its ancestors.
 //
 // In particular, in the case of L1 extension, the L2 unsafe head will generally remain the same,
 // but in the case of an L1 re-org, we need to search for the new safe and unsafe L2 block.
@@ -36,7 +39,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum"
 
 	"github.com/ethereum/go-ethereum/common"
 
@@ -58,16 +60,21 @@ var TooDeepReorgErr = errors.New("reorg is too deep")
 
 const MaxReorgDepth = 500
 
-// isCanonical returns true if the supplied block ID is canonical in the L1 chain.
-// If the L1 block is not found by number (ethereum.NotFound), the error will be nil.
-func isCanonical(ctx context.Context, l1 L1Chain, block eth.BlockID) (bool, error) {
-	canonical, err := l1.L1BlockRefByNumber(ctx, block.Number)
-	if err != nil && !errors.Is(err, ethereum.NotFound) {
-		return false, err
-	} else if err != nil {
-		return false, nil
+// isCanonical returns the following values:
+// - `aheadOrCanonical: true if the supplied block is ahead of the known head of the L1 chain,
+//    or canonical in the L1 chain.
+// - `canonical`: true if the block is canonical in the L1 chain.
+func isAheadOrCanonical(ctx context.Context, l1 L1Chain, block eth.BlockID) (aheadOrCanonical bool, canonical bool, err error) {
+	if l1Head, err := l1.L1HeadBlockRef(ctx); err != nil {
+		return false, false, err
+	} else if block.Number > l1Head.Number {
+		return true, false, nil
+	} else if canonical, err := l1.L1BlockRefByNumber(ctx, block.Number); err != nil {
+		return false, false, err
+	} else {
+		canonical := canonical.Hash == block.Hash
+		return canonical, canonical, nil
 	}
-	return canonical.Hash == block.Hash, nil
 }
 
 // FindL2Heads walks back from `start` (the previous unsafe L2 block) and finds the unsafe and safe
@@ -79,27 +86,12 @@ func isCanonical(ctx context.Context, l1 L1Chain, block eth.BlockID) (bool, erro
 //       complete within the canonical L1 chain (as known to the opnode).
 //
 // (1) Plausible meaning that the blockhash of the L2 block's L1 origin (as reported in the L1
-//     Attributes deposit within the L2 block) is not canonical at another height in the L1 chain.
+//     Attributes deposit within the L2 block) is not canonical at another height in the L1 chain,
+//     and the same holds for all its ancestors.
 func FindL2Heads(ctx context.Context, start eth.L2BlockRef, seqWindowSize uint64,
 	l1 L1Chain, l2 L2Chain, genesis *rollup.Genesis) (unsafe eth.L2BlockRef, safe eth.L2BlockRef, err error) {
 
-	// First check if the L1 origin of the start block is ahead of the current L1 head.
-	// If so, we assume that this should be the next unsafe head for the sequencing window.
-	// We still need to walk back the safe head because we don't know where the reorg started.
-	l1Head, err := l1.L1HeadBlockRef(ctx)
-	if err != nil {
-		return eth.L2BlockRef{}, eth.L2BlockRef{}, err
-	}
-	l2Ahead := start.L1Origin.Number > l1Head.Number
-
-	// The loop that follows walks the L2 chain until we find the "latest" L2 block.
-	// This the first L2 block whose L1 origin is canonical.
-	var latest eth.L2BlockRef
-
-	// L1 origin hash during for the L2 block during the previous iteration, 0 for first iteration.
-	// When this changes as we walk the L2 chain backwards, it means we're seeing a different
-	// (earlier) epoch.
-	var prevL1OriginHash common.Hash
+	// Loop 1. Walk the L2 chain backwards until we find an L2 block whose L1 origin is canonical.
 
 	// Current L2 block.
 	n := start
@@ -107,20 +99,35 @@ func FindL2Heads(ctx context.Context, start eth.L2BlockRef, seqWindowSize uint64
 	// Number of blocks between n and start.
 	reorgDepth := 0
 
+	// Blockhash of L1 origin hash for the L2 block during the previous iteration, 0 for first
+	// iteration. When this changes as we walk the L2 chain backwards, it means we're seeing a different
+	// (earlier) epoch.
+	var prevL1OriginHash common.Hash
+
+	// The highest L2 ancestor of `start` (or `start` itself) whose ancestors are not (yet) known
+	// to have a non-canonical L1 origin. Empty if no such candidate is known yet. Guaranteed to be
+	// set after exiting from Loop 1.
+	var highestPlausibleCanonicalOrigin eth.L2BlockRef
+
 	for {
 		// Check if l1Origin is canonical when we get to a new epoch.
 		if prevL1OriginHash != n.L1Origin.Hash {
-			if ok, err := isCanonical(ctx, l1, n.L1Origin); err != nil {
+			prevL1OriginHash = n.L1Origin.Hash
+
+			if plausible, canonical, err := isAheadOrCanonical(ctx, l1, n.L1Origin); err != nil {
 				return eth.L2BlockRef{}, eth.L2BlockRef{}, err
-			} else if ok {
-				// L1 block is canonical, "latest" L2 block found.
-				latest = n
-				prevL1OriginHash = n.L1Origin.Hash
-				break
+			} else if !plausible {
+				// L1 origin nor ahead of L1 head nor canonical, discard previous candidate and
+				// keep looking.
+				highestPlausibleCanonicalOrigin = eth.L2BlockRef{}
 			} else {
-				// L1 block not canonical (either we don't know an L1 block at this height, or
-				// have another block hash for it), keep looking.
-				prevL1OriginHash = n.L1Origin.Hash
+				if highestPlausibleCanonicalOrigin == (eth.L2BlockRef{}) {
+					// No highest plausible candidate, make L2 block new candidate.
+					highestPlausibleCanonicalOrigin = n
+				}
+				if canonical {
+					break
+				}
 			}
 		}
 
@@ -149,16 +156,19 @@ func FindL2Heads(ctx context.Context, start eth.L2BlockRef, seqWindowSize uint64
 		}
 	}
 
-	// Walk from the L1 origin of the "latest" block back to the L1 block that starts the sequencing
-	// window ending at that block. Instead of iterating on L1 blocks, we actually iterate on L2
-	// blocks, because we want to find the safe head, i.e. the highest L2 block whose L1 origin
-	// is the start of the sequencing window.
+	// Loop 2. Walk from the L1 origin of the `n` block (*) back to the L1 block that starts the
+	// sequencing window ending at that block. Instead of iterating on L1 blocks, we actually
+	// iterate on L2 blocks, because we want to find the safe L2 head, i.e. the highest L2 block
+	// whose L1 origin is the start of the sequencing window.
+
+	// (*) `n` being at this stage the highest L2 block whose L1 origin is canonical.
 
 	// Depth counter: we need to walk back `seqWindowSize` L1 blocks in order to find the start
 	// of the sequencing window.
 	depth := uint64(1)
 
-	// Before entering the loop: n == latest && prevL1OriginHash == n.L1Origin.Hash
+	// Before entering the loop: `prevL1OriginHash == n.L1Origin.Hash`
+	// The original definitions of `n` and `prevL1OriginHash` still hold.
 	for {
 		// Advance depth if we change to a different (earlier) epoch.
 		if n.L1Origin.Hash != prevL1OriginHash {
@@ -168,18 +178,14 @@ func FindL2Heads(ctx context.Context, start eth.L2BlockRef, seqWindowSize uint64
 
 		// Found an L2 block whose L1 origin is the start of the sequencing window.
 		if depth == seqWindowSize {
-			if l2Ahead {
-				return start, n, nil
-			} else {
-				return latest, n, nil
-			}
+			return highestPlausibleCanonicalOrigin, n, nil
 		}
 
 		// Genesis is always safe.
 		if n.Hash == genesis.L2.Hash || n.Number == genesis.L2.Number {
 			safe = eth.L2BlockRef{Hash: genesis.L2.Hash, Number: genesis.L2.Number,
 				Time: genesis.L2Time, L1Origin: genesis.L1}
-			return start, safe, nil
+			return highestPlausibleCanonicalOrigin, safe, nil
 		}
 
 		// Pull L2 parent for next iteration.
