c441f7faf4a5ce06f03ff3dc100acbcdd64485d7
Maurelian <maurelian@protonmail.ch>
2022-04-23 09:57:52 -0400

    test: Add sendMessage and pause tests for L1xDM

--- packages/contracts/contracts/test/L1CrossDomainMessenger.t.sol
+++ packages/contracts/contracts/test/L1CrossDomainMessenger.t.sol
@@ -1,18 +1,84 @@
-// pause: should pause the contract when called by the current owner
-// pause: should not pause the contract when called by account other than the owner
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
 
-// sendMessage: should be able to send a single message
-// sendMessage: should be able to send the same message twice
+/* Testing utilities */
+import { CommonTest } from "./CommonTest.sol";
+import { L2OutputOracle_Initializer } from "./L2OutputOracle.t.sol";
 
-// replayMessage: when giving some incorrect input value: should revert if given the wrong target
-// replayMessage: when giving some incorrect input value: should revert if given the wrong sender
-// replayMessage: when giving some incorrect input value: should revert if given the wrong message
-// replayMessage: when giving some incorrect input value: should revert if given the wrong queue index
-// replayMessage: when giving some incorrect input value: should revert if given the wrong old gas limit
+import {
+    Lib_DefaultValues
+} from "@eth-optimism/contracts/libraries/constants/Lib_DefaultValues.sol";
+import {
+    Lib_PredeployAddresses
+} from "@eth-optimism/contracts/libraries/constants/Lib_PredeployAddresses.sol";
+import {
+    Lib_CrossDomainUtils
+} from "@eth-optimism/contracts/libraries/bridge/Lib_CrossDomainUtils.sol";
 
-// replayMessage: when all input values are the same as the existing message: should succeed
-// replayMessage: when all input values are the same as the existing message: should emit the TransactionEnqueued event
-// replayMessage: when all input values are the same as the existing message: should succeed if all inputs are the same as the existing message
+/* Target contract dependencies */
+import { L2OutputOracle } from "../L1/L2OutputOracle.sol";
+import { OptimismPortal } from "../L1/OptimismPortal.sol";
+
+/* Target contract */
+import { L1CrossDomainMessenger } from "../L1/messaging/L1CrossDomainMessenger.sol";
+
+contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
+    // Dependencies
+    OptimismPortal op;
+    // 'L2OutputOracle oracle' is declared in L2OutputOracle_Initializer
+
+    // Contract under test
+    L1CrossDomainMessenger messenger;
+
+    // Receiver address for testing
+    address recipient = address(0xabbaacdc);
+
+    function setUp() external {
+        // Oracle value is zero, but this test does not depend on it.
+        op = new OptimismPortal(oracle, 7 days);
+        messenger = new L1CrossDomainMessenger();
+        messenger.initialize(op);
+    }
+
+    // pause: should pause the contract when called by the current owner
+    function test_pause() external {
+        messenger.pause();
+        assert(messenger.paused());
+    }
+
+    // pause: should not pause the contract when called by account other than the owner
+    function testCannot_pause() external {
+        vm.expectRevert('Ownable: caller is not the owner');
+        vm.prank(address(0xABBA));
+        messenger.pause();
+    }
+
+    // sendMessage: should be able to send a single message
+    function test_sendMessage() external {
+        uint256 messageNonce = messenger.messageNonce();
+        bytes memory xDomainCalldata = Lib_CrossDomainUtils.encodeXDomainCalldata(
+            recipient,
+            address(this),
+            NON_ZERO_DATA,
+            messageNonce
+        );
+        vm.expectCall(
+            address(op),
+            abi.encodeWithSignature(
+                "depositTransaction(address,uint256,uint256,bool,bytes)",
+                Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER, 0, NON_ZERO_GASLIMIT, false, xDomainCalldata
+            )
+        );
+        messenger.sendMessage(recipient, NON_ZERO_DATA, uint32(NON_ZERO_GASLIMIT));
+    }
+
+    // sendMessage: should be able to send the same message twice
+    function test_sendMessageTwice() external {
+        messenger.sendMessage(recipient, NON_ZERO_DATA, uint32(NON_ZERO_GASLIMIT));
+        messenger.sendMessage(recipient, NON_ZERO_DATA, uint32(NON_ZERO_GASLIMIT));
+    }
+
+
 
 // xDomainMessageSender: should return the xDomainMsgSender address
 
