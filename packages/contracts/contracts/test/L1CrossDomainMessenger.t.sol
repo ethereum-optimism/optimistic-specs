0a1eb691174723e5af18c14c6b4d29a7d54b9371
Mark Tyneway <mark.tyneway@gmail.com>
2022-04-28 15:10:33 -0700

    test: L1CrossDomainMessenger

--- packages/contracts/contracts/test/L1CrossDomainMessenger.t.sol
+++ packages/contracts/contracts/test/L1CrossDomainMessenger.t.sol
@@ -24,10 +24,23 @@ import { OptimismPortal } from "../L1/OptimismPortal.sol";
 /* Target contract */
 import { L1CrossDomainMessenger } from "../L1/messaging/L1CrossDomainMessenger.sol";
 
+import {
+    ICrossDomainMessenger
+} from "@eth-optimism/contracts/libraries/bridge/ICrossDomainMessenger.sol";
+
 contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
     // Dependencies
     OptimismPortal op;
     // 'L2OutputOracle oracle' is declared in L2OutputOracle_Initializer
+
+    event SentMessage(
+        address indexed target,
+        address sender,
+        bytes message,
+        uint256 messageNonce,
+        uint256 gasLimit
+    );
+    event RelayedMessage(bytes32 indexed msgHash);
 
     // Contract under test
     L1CrossDomainMessenger messenger;
@@ -36,8 +49,8 @@ contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
     address recipient = address(0xabbaacdc);
 
     function setUp() external {
-        // new portal with zero finalization window
-        op = new OptimismPortal(oracle, 0);
+        // new portal with small finalization window
+        op = new OptimismPortal(oracle, 100);
         messenger = new L1CrossDomainMessenger();
         messenger.initialize(op, Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER);
     }
@@ -85,6 +98,8 @@ contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
     }
 
     // xDomainMessageSender: should return the xDomainMsgSender address
+    // TODO: might need a test contract
+    // function test_xDomainSenderSetCorrectly() external {}
 
     // relayMessage: should send a successful call to the target contract
     function test_relayMessageSucceeds() external {
@@ -115,6 +130,8 @@ contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
         // set the value of op.l2Sender() to be the L2 Cross Domain Messenger.
         vm.store(address(op), 0, bytes32(abi.encode(sender)));
         vm.prank(address(op));
+        vm.expectEmit(true, true, true, true);
+        emit RelayedMessage(keccak256(xDomainCalldata));
         messenger.relayMessage(target, sender, message, messageNonce);
 
         // Ensure the hash of the xDomainCalldata was stored in the successfulMessages mapping.
@@ -122,15 +139,179 @@ contract L1CrossDomainMessenger_Test is CommonTest, L2OutputOracle_Initializer {
         assert(messenger.successfulMessages(messageHash));
     }
 
+
     // relayMessage: should revert if still inside the fraud proof window
+    function test_relayMessageInsideFraudProofWindow() external {
+        bytes memory cd = abi.encodeWithSelector(
+            L1CrossDomainMessenger.relayMessage.selector,
+            address(42),
+            address(this),
+            hex"1111",
+            0
+        );
+
+         WithdrawalVerifier.OutputRootProof memory outputRootProof = WithdrawalVerifier.OutputRootProof({
+            version: bytes32(0),
+            stateRoot: bytes32(0),
+            withdrawerStorageRoot: bytes32(0),
+            latestBlockhash:bytes32(0)
+        });
+
+        bytes memory withdrawProof = bytes(hex"");
+
+        // get the finalization window
+        uint256 window = op.FINALIZATION_PERIOD();
+        assert(window != 0);
+        // set block.timestamp to be one less than the finalization window.
+        // the timestamp 0 is passed into `finalizeWithdrawalTransaction`
+        vm.warp(window - 1);
+
+        // The OptimismPortal is responsible for keeping track
+        // of the finalization window
+        vm.expectRevert(abi.encodeWithSignature("NotYetFinal()"));
+        op.finalizeWithdrawalTransaction(
+            0,               // nonce
+            address(this),   // sender
+            address(42),     // target
+            0,               // value
+            100000,          // gasLimit
+            cd,              // calldata
+            0,               // timestamp
+            outputRootProof, // outputRootProof
+            withdrawProof    // withdrawProof
+        );
+    }
+
     // relayMessage: should revert if attempting to relay a message sent to an L1 system contract
+    function test_relayMessageToSystemContract() external {
+        // set the target to be the OptimismPortal
+        address target = address(op);
+        address sender = Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER;
+        bytes memory message = hex"1111";
+        uint256 messageNonce = 42;
+
+        // set the value of op.l2Sender() to be the L2 Cross Domain Messenger.
+        vm.store(address(op), 0, bytes32(abi.encode(sender)));
+        vm.prank(address(op));
+        vm.expectRevert("Cannot send L2->L1 messages to L1 system contracts.");
+        messenger.relayMessage(target, sender, message, messageNonce);
+    }
+
     // relayMessage: should revert if provided an invalid output root proof
-    // relayMessage: should revert if provided an invalid storage trie witness
+    function test_revertOnInvalidOutputRootProof() external {
+        // create an invalid output root proof
+        WithdrawalVerifier.OutputRootProof memory outputRootProof = WithdrawalVerifier.OutputRootProof({
+            version: bytes32(0),
+            stateRoot: bytes32(0),
+            withdrawerStorageRoot: bytes32(0),
+            latestBlockhash:bytes32(0)
+        });
+        bytes memory withdrawProof = bytes(hex"");
+
+        vm.expectRevert(abi.encodeWithSignature("InvalidOutputRootProof()"));
+        op.finalizeWithdrawalTransaction(
+            0,               // nonce
+            address(this),   // sender
+            address(42),     // target
+            0,               // value
+            100000,          // gasLimit
+            bytes(""),       // calldata
+            0,               // timestamp
+            outputRootProof, // outputRootProof
+            withdrawProof    // withdrawProof
+        );
+    }
+
     // relayMessage: the xDomainMessageSender is reset to the original value
+    function test_xDomainMessageSenderResets() external {
+        vm.expectRevert("xDomainMessageSender is not set");
+        messenger.xDomainMessageSender();
+
+        address sender = Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER;
+        bytes memory message = hex"1111";
+        uint256 messageNonce = 42;
+
+        vm.store(address(op), 0, bytes32(abi.encode(sender)));
+        vm.prank(address(op));
+        messenger.relayMessage(address(0), sender, message, messageNonce);
+
+        vm.expectRevert("xDomainMessageSender is not set");
+        messenger.xDomainMessageSender();
+    }
+
     // relayMessage: should revert if trying to send the same message twice
+    function test_relayShouldRevertSendingSameMessageTwice() external {
+        // TODO: this is a test on the L2CrossDomainMessenger
+    }
+
     // relayMessage: should revert if paused
+    function test_relayShouldRevertIfPaused() external {
+        vm.prank(messenger.owner());
+        messenger.pause();
+
+        vm.expectRevert("Pausable: paused");
+        messenger.relayMessage(address(0), address(0), hex"", 0);
+    }
 
     // blockMessage and allowMessage: should revert if called by an account other than the owner
+    function test_relayMessageBlockingAuth() external {
+        bytes32 msgHash = bytes32(hex"ff");
+
+        vm.prank(address(0));
+        vm.expectRevert("Ownable: caller is not the owner");
+        messenger.blockMessage(msgHash);
+        assert(messenger.blockedMessages(msgHash) == false);
+
+        vm.prank(address(0));
+        vm.expectRevert("Ownable: caller is not the owner");
+        messenger.allowMessage(msgHash);
+        assert(messenger.blockedMessages(msgHash) == false);
+    }
+
     // blockMessage and allowMessage: should revert if the message is blocked
+    function test_relayRevertOnBlockedMessage() external {
+        bytes memory xDomainCalldata = Lib_CrossDomainUtils.encodeXDomainCalldata(
+            address(0),
+            address(0),
+            hex"ff",
+            0
+        );
+        bytes32 msgHash = keccak256(xDomainCalldata);
+
+        vm.prank(messenger.owner());
+        messenger.blockMessage(msgHash);
+
+        vm.store(address(op), 0, bytes32(abi.encode(Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER)));
+        vm.prank(address(op));
+        vm.expectRevert("Provided message has been blocked.");
+        messenger.relayMessage(address(0), address(0), hex"ff", 0);
+    }
+
     // blockMessage and allowMessage: should succeed if the message is blocked, then unblocked
+    function test_blockAndUnblockSuccessfulMessage() external {
+        bytes memory xDomainCalldata = Lib_CrossDomainUtils.encodeXDomainCalldata(
+            address(0),
+            address(0),
+            hex"ff",
+            0
+        );
+        bytes32 msgHash = keccak256(xDomainCalldata);
+
+        vm.prank(messenger.owner());
+        messenger.blockMessage(msgHash);
+
+        vm.store(address(op), 0, bytes32(abi.encode(Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER)));
+        vm.prank(address(op));
+        vm.expectRevert("Provided message has been blocked.");
+        messenger.relayMessage(address(0), address(0), hex"ff", 0);
+
+        vm.prank(messenger.owner());
+        messenger.allowMessage(msgHash);
+
+        vm.prank(address(op));
+
+        vm.expectEmit(true, true, true, true);
+        emit RelayedMessage(msgHash);
+        messenger.relayMessage(address(0), address(0), hex"ff", 0);
+    }
 }
