b72fe11f695e6c0458f98db5fa364b47b4f98e84
Mark Tyneway <mark.tyneway@gmail.com>
2022-04-29 11:57:18 -0700

    contracts: test infra for bridge

--- packages/contracts/contracts/test/L2StandardBridge.t.sol
+++ packages/contracts/contracts/test/L2StandardBridge.t.sol
@@ -1,18 +1,36 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import { CommonTest } from "./CommonTest.sol";
-import { L2OutputOracle_Initializer } from "./L2OutputOracle.t.sol";
+import {
+    Lib_PredeployAddresses
+} from "@eth-optimism/contracts/libraries/constants/Lib_PredeployAddresses.sol";
 
+import { IWithdrawer } from "../L2/IWithdrawer.sol";
+import { Withdrawer } from "../L2/Withdrawer.sol";
 import { L2StandardBridge } from "../L2/messaging/L2StandardBridge.sol";
 import { L1StandardBridge } from "../L1/messaging/L1StandardBridge.sol";
 import { OptimismPortal } from "../L1/OptimismPortal.sol";
+import { Lib_BedrockPredeployAddresses } from "../libraries/Lib_BedrockPredeployAddresses.sol";
+import { L2StandardTokenFactory } from "../L2/messaging/L2StandardTokenFactory.sol";
+import { IL2StandardTokenFactory } from "../L2/messaging/IL2StandardTokenFactory.sol";
+import { L2StandardERC20 } from "../L2/tokens/L2StandardERC20.sol";
+import { IL2StandardERC20 } from "../L2/tokens/IL2StandardERC20.sol";
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { CommonTest } from "./CommonTest.sol";
+import { L2OutputOracle_Initializer } from "./L2OutputOracle.t.sol";
+import { LibRLP } from "./Lib_RLP.sol";
+
+import { console } from "forge-std/console.sol";
 
 contract L2StandardBridge_Test is CommonTest, L2OutputOracle_Initializer {
     OptimismPortal op;
 
-    L1StandardBridge L1Bridge ;
-    L2StandardBridge L2Bridge ;
+    IWithdrawer W;
+    L1StandardBridge L1Bridge;
+    L2StandardBridge L2Bridge;
+    IL2StandardTokenFactory L2TokenFactory;
+    IL2StandardERC20 L2Token;
 
     function setUp() external {
         L1Bridge = new L1StandardBridge();
@@ -20,6 +38,27 @@ contract L2StandardBridge_Test is CommonTest, L2OutputOracle_Initializer {
         op = new OptimismPortal(oracle, 100);
 
         L1Bridge.initialize(op, address(L2Bridge));
+
+        Withdrawer w = new Withdrawer();
+        vm.etch(Lib_BedrockPredeployAddresses.WITHDRAWER, address(w).code);
+        W = IWithdrawer(Lib_BedrockPredeployAddresses.WITHDRAWER);
+
+        L2StandardTokenFactory factory = new L2StandardTokenFactory();
+        vm.etch(Lib_PredeployAddresses.L2_STANDARD_TOKEN_FACTORY, address(factory).code);
+        L2TokenFactory = IL2StandardTokenFactory(Lib_PredeployAddresses.L2_STANDARD_TOKEN_FACTORY);
+
+        ERC20 token = new ERC20("Test Token", "TT");
+
+        // Deploy the L2 ERC20 now
+        L2TokenFactory.createStandardL2Token(
+            address(token),
+            string(abi.encodePacked("L2-", token.name())),
+            string(abi.encodePacked("L2-", token.symbol()))
+        );
+
+        L2Token = IL2StandardERC20(
+            LibRLP.computeAddress(address(L2TokenFactory), 0)
+        );
     }
 
     function test_L2BridgeCorrectL1Bridge() external {
@@ -27,7 +66,6 @@ contract L2StandardBridge_Test is CommonTest, L2OutputOracle_Initializer {
         assertEq(address(L1Bridge), l1Bridge);
     }
 
-    // l1TokenBridge is correct
     // withdraw
     // - token is burned
     // - emits WithdrawalInitiated
