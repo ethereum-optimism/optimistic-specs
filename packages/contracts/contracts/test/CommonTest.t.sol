8024ecdb809d14c74d677e3772b807e5b0525ab3
Mark Tyneway <mark.tyneway@gmail.com>
2022-05-01 20:27:35 -0700

    contracts: clean up common test

--- packages/contracts/contracts/test/CommonTest.t.sol
+++ packages/contracts/contracts/test/CommonTest.t.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.10;
 
 /* Testing utilities */
 import { Test } from "forge-std/Test.sol";
-import { Vm } from "forge-std/Vm.sol";
 
 contract CommonTest is Test {
     address immutable ZERO_ADDRESS = address(0);
