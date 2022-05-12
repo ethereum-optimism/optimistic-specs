36674ccb55584168a477a48bd8a34c1687772ab9
Mark Tyneway <mark.tyneway@gmail.com>
2022-05-12 16:59:40 -0700

    contracts: more metering work

--- packages/contracts/contracts/L1/GasMetering.sol
+++ packages/contracts/contracts/L1/GasMetering.sol
@@ -18,60 +18,74 @@ abstract contract GasMetering {
     bytes32 internal storage1;
 
     // gaslimit, sane gas limit
+    // TODO: need a method for setting these values
     bytes32 internal storage2;
 
-    uint256 constant internal ELASTICITY_MULTIPLIER = 2;
-    uint256 constant internal BASE_FEE_MAX_CHANGE_DENOMINATOR = 8;
+    uint8 constant internal ELASTICITY_MULTIPLIER = 2;
+    uint8 constant internal BASE_FEE_MAX_CHANGE_DENOMINATOR = 8;
+
+    function _initialize(
+        uint128 prevBaseFee,
+        uint64 prevNum,
+        uint64 prevBoughtGas,
+        uint128 gasTargetLimit,
+        uint128 gasSanityLimit
+    ) internal {
+        require(storage1 == bytes32(0) && storage2 == bytes32(0));
+
+        storage1 = SlotPacking128x64x64.set(
+            prevBaseFee,
+            prevNum,
+            prevBoughtGas
+        );
+
+        storage2 = SlotPacking128x128.set(
+            gasTargetLimit,
+            gasSanityLimit
+        );
+    }
 
     function _burnGas(uint256 amount) internal view {
         uint256 i = 0;
         uint256 gas = gasleft();
-        uint256 target = gas - amount;
-        while (target < gasleft()) {
-            i++;
+        while (gas - amount < gasleft()) {
+            ++i;
         }
     }
 
     // TODO: rename variables + update spec with renamed variables
-    modifier gasMetered(uint256 requestedGas, uint256 mint) {
-        // prev_basefee, prev_num, prev_bought_gas = load_and_unpack_storage()
+    function gasMetered(uint64 requestedGas, uint256 mint) internal {
         (uint128 prevBaseFee, uint64 prevNum, uint64 prevBoughtGas) =
             SlotPacking128x64x64.get(storage1);
 
-        // constants
-        // gas_target_limit, gas_sanity_limit = load_and_unpack_storage2()
         (uint128 gasTargetLimit, uint128 gasSanityLimit) =
             SlotPacking128x128.get(storage2);
-        uint256 gasTarget = gasTargetLimit / ELASTICITY_MULTIPLIER;
+        uint128 gasTarget = gasTargetLimit / ELASTICITY_MULTIPLIER;
 
         // business logic below
-
-        uint256 gasCost = requestedGas * prevBaseFee;
-
-        uint256 nowBaseFee = prevBaseFee;
-        uint256 nowNum = block.number;
-        uint256 nowBoughtGas =  prevBoughtGas + requestedGas;
+        uint128 nowBaseFee = prevBaseFee;
+        uint64 nowBoughtGas =  prevBoughtGas + requestedGas;
+        uint64 nowNum = uint64(block.number);
 
         if (nowNum != prevNum) {
-            // TODO: this is in the spec
-            // uint256 nowBoughtGas = requestedGas;
-            if (prevBoughtGas < gasTarget) {
-                uint256 gasUsedDelta = prevBoughtGas - gasTarget;
-                uint256 baseFeePerGasDelta =
+            nowBoughtGas = requestedGas;
+            if (prevBoughtGas > gasTarget) {
+                uint128 gasUsedDelta = prevBoughtGas - gasTarget;
+                uint128 baseFeePerGasDelta =
                     prevBaseFee * gasUsedDelta / gasTarget / BASE_FEE_MAX_CHANGE_DENOMINATOR;
                 if (baseFeePerGasDelta == 0) {
                     baseFeePerGasDelta = 1;
                 }
                 nowBaseFee = prevBaseFee - baseFeePerGasDelta;
-            } else if (prevBoughtGas > gasTarget) {
-                uint256 gasUsedDelta = gasTarget - prevBoughtGas;
-                uint256 baseFeePerGasDelta =
+            } else if (prevBoughtGas < gasTarget) {
+                uint128 gasUsedDelta = gasTarget - prevBoughtGas;
+                uint128 baseFeePerGasDelta =
                     prevBaseFee * gasUsedDelta / gasTarget / BASE_FEE_MAX_CHANGE_DENOMINATOR;
                 nowBaseFee = prevBaseFee - baseFeePerGasDelta;
             }
         }
 
-        require(nowBoughtGas < gasSanityLimit);
+        require(nowBoughtGas < gasSanityLimit, "Cannot buy more L2 gas.");
         uint256 requiredLockup = mint + (requestedGas * nowBaseFee);
 
         // in payable version:
@@ -81,11 +95,9 @@ abstract contract GasMetering {
 
         // TODO: these type conversions are unsafe
         storage1 = SlotPacking128x64x64.set(
-            uint128(nowBaseFee),
-            uint64(nowNum),
-            uint64(nowBoughtGas)
+            nowBaseFee,
+            nowNum,
+            nowBoughtGas
         );
-
-        _;
     }
 }
