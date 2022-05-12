5d1bc5f9b79c4faf98be068cc584c5d5b4bea21f
Matthew Slipper <me@matthewslipper.com>
2022-05-12 08:49:30 -0600

    Code review updates

--- packages/contracts/contracts/L2/OVM_L1BlockNumber.sol
+++ packages/contracts/contracts/L2/OVM_L1BlockNumber.sol
@@ -2,12 +2,18 @@
 pragma solidity 0.8.10;
 
 import { iOVM_L1BlockNumber } from "./iOVM_L1BlockNumber.sol";
-import { L1Block } from "../L1Block.sol";
+import { L1Block } from "./L1Block.sol";
 
 contract OVM_L1BlockNumber is iOVM_L1BlockNumber {
-    address public l1BlockAddress;
+    address internal l1BlockAddress;
 
-    constructor(address _l1BlockAddress) {
+    error AlreadyInitialized();
+
+    function initialize(address _l1BlockAddress) external {
+        if (l1BlockAddress != address(0)) {
+            revert AlreadyInitialized();
+        }
+
         l1BlockAddress = _l1BlockAddress;
     }
 
--- packages/contracts/contracts/test/OVM_L1BlockNumber.t.sol
+++ packages/contracts/contracts/test/OVM_L1BlockNumber.t.sol
@@ -1,22 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import { DSTest } from "forge-std/Test.sol";
-import { Vm } from "forge-std/Vm.sol";
+import { Test } from "forge-std/Test.sol";
 import { L1Block } from "../L2/L1Block.sol";
-import { OVM_L1BlockNumber } from "../L2/predeploys/OVM_L1BlockNumber.sol";
+import { OVM_L1BlockNumber } from "../L2/OVM_L1BlockNumber.sol";
 
-contract OVM_L1BlockNumberTest is DSTest {
-    Vm vm = Vm(HEVM_ADDRESS);
+contract OVM_L1BlockNumberTest is Test {
     L1Block lb;
     OVM_L1BlockNumber bn;
     bytes32 immutable NON_ZERO_HASH = keccak256(abi.encode(1));
 
     function setUp() external {
         lb = new L1Block();
-        bn = new OVM_L1BlockNumber(address(lb));
+        bn = new OVM_L1BlockNumber();
         vm.prank(lb.DEPOSITOR_ACCOUNT());
         lb.setL1BlockValues(uint64(999), uint64(2), 3, NON_ZERO_HASH, uint64(4));
+        bn.initialize(address(lb));
+    }
+
+    function test_initializeCalledOnce() external {
+        vm.expectRevert(abi.encodeWithSignature("AlreadyInitialized()"));
+        bn.initialize(address(1));
     }
 
     function test_getL1BlockNumber() external {
