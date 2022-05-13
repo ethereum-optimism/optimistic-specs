6c6e8edf6201735782d50ebc55af0dee86df710b
Mark Tyneway <mark.tyneway@gmail.com>
2022-05-12 17:10:13 -0700

    contracts: clean up lib

--- packages/contracts/contracts/libraries/SlotPacking128x128.sol
+++ packages/contracts/contracts/libraries/SlotPacking128x128.sol
@@ -4,11 +4,9 @@ pragma solidity 0.8.10;
 library SlotPacking128x128 {
     function set(uint128 a, uint128 b) internal pure returns (bytes32 slot) {
         assembly {
-            slot := or(slot, a)
-            slot := shl(128, slot)
+            slot := or(slot, shl(128, a))
             slot := or(slot, b)
         }
-        return slot;
     }
 
     function get(bytes32 slot) internal pure returns (uint128 a, uint128 b) {
