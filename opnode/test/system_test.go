20143e9030f6cd3033d05e4e6e115a790cc8b46b
Joshua Gutow <jgutow@optimism.io>
2022-05-13 10:31:42 -0700

    opnode/test: Remove dead code

--- opnode/test/system_test.go
+++ opnode/test/system_test.go
@@ -1,9 +1,7 @@
 package test
 
 import (
-	"bytes"
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"math/big"
@@ -34,11 +32,8 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/trie"
 	"github.com/stretchr/testify/require"
 )
 
@@ -651,92 +646,7 @@ func TestL1InfoContract(t *testing.T) {
 
 }
 
-type proofDB struct {
-	m map[string][]byte
-}
-
-func (p *proofDB) Has(key []byte) (bool, error) {
-	_, ok := p.m[string(key)]
-	return ok, nil
-}
-
-func (p *proofDB) Get(key []byte) ([]byte, error) {
-	v, ok := p.m[string(key)]
-	if !ok {
-		return nil, errors.New("not found")
-	}
-	return v, nil
-}
-
-func GenerateProofDB(proof []string) *proofDB {
-	p := proofDB{m: make(map[string][]byte)}
-	for _, s := range proof {
-		value := common.FromHex(s)
-		key := crypto.Keccak256(value)
-		p.m[string(key)] = value
-	}
-	return &p
-}
-
-func VerifyAccountProof(root common.Hash, address common.Address, account types.StateAccount, proof []string) error {
-	expected, err := rlp.EncodeToBytes(&account)
-	if err != nil {
-		return fmt.Errorf("failed to encode rlp: %w", err)
-	}
-	secureKey := crypto.Keccak256(address[:])
-	db := GenerateProofDB(proof)
-	value, err := trie.VerifyProof(root, secureKey, db)
-	if err != nil {
-		return fmt.Errorf("failed to verify proof: %w", err)
-	}
-
-	if bytes.Equal(value, expected) {
-		return nil
-	} else {
-		return errors.New("proved value is not the same as the expected value")
-	}
-}
-
-func VerifyStorageProof(root common.Hash, proof gethclient.StorageResult) error {
-	secureKey := crypto.Keccak256(common.FromHex(proof.Key))
-	db := GenerateProofDB(proof.Proof)
-	value, err := trie.VerifyProof(root, secureKey, db)
-	if err != nil {
-		return fmt.Errorf("failed to verify proof: %w", err)
-	}
-
-	expected := proof.Value.Bytes()
-	if bytes.Equal(value, expected) {
-		return nil
-	} else {
-		return errors.New("proved value is not the same as the expected value")
-	}
-}
-
-func VerifyProof(stateRoot common.Hash, proof *gethclient.AccountResult) error {
-	err := VerifyAccountProof(
-		stateRoot,
-		proof.Address,
-		types.StateAccount{
-			Nonce:    proof.Nonce,
-			Balance:  proof.Balance,
-			Root:     proof.StorageHash,
-			CodeHash: proof.CodeHash[:],
-		},
-		proof.AccountProof,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to validate account: %w", err)
-	}
-	for i, storageProof := range proof.StorageProof {
-		err = VerifyStorageProof(proof.StorageHash, storageProof)
-		if err != nil {
-			return fmt.Errorf("failed to validate storage proof %d: %w", i, err)
-		}
-	}
-	return nil
-}
-
+// calcGasFees determines the actual cost of the transaction given a specific basefee
 func calcGasFees(gasUsed uint64, gasTipCap *big.Int, gasFeeCap *big.Int, baseFee *big.Int) *big.Int {
 	x := new(big.Int).Add(gasTipCap, baseFee)
 	// If tip + basefee > gas fee cap, clamp it to the gas fee cap
